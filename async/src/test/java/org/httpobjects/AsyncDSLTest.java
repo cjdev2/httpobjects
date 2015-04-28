a003b68c36d6e359c36e73bf5e13d0045e3ee16c
Chris Albright <calbright@cj.com>
2015-04-27 22:09:36 -0700

    starting work on async wrapper

---
+++ async/src/test/java/org/httpobjects/AsyncDSLTest.java
@@ -0,0 +1,13 @@
+package org.httpobjects;
+
+import org.junit.Test;
+import scala.concurrent.ExecutionContext;
+
+public class AsyncDSLTest {
+  ExecutionContext ctx = ExecutionContext.Implicits.global();
+
+  @Test
+  public void testAsyncDsl() {
+    
+  }
+}
