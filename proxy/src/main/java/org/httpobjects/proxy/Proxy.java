a1439d2c30db40d5be197a0edf0a770ec40a37f6
SeanShubin <sshubin@cj.com>
2015-08-07 14:21:53 -0700

    Added PATCH to proxy

--- proxy/src/main/java/org/httpobjects/proxy/Proxy.java
+++ proxy/src/main/java/org/httpobjects/proxy/Proxy.java
@@ -37,145 +37,136 @@
  */
 package org.httpobjects.proxy;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethodBase;
-import org.apache.commons.httpclient.URI;
-import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.methods.DeleteMethod;
-import org.apache.commons.httpclient.methods.EntityEnclosingMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.httpobjects.HttpObject;
-import org.httpobjects.Representation;
-import org.httpobjects.Request;
-import org.httpobjects.Response;
-import org.httpobjects.ResponseCode;
+import org.httpobjects.*;
 import org.httpobjects.header.GenericHeaderField;
 import org.httpobjects.header.HeaderField;
 import org.httpobjects.header.response.LocationField;
 import org.httpobjects.header.response.SetCookieField;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Proxy extends HttpObject {
-	private final Log log = LogFactory.getLog(getClass());
-	private String base;
-	private final String me;
-	
-	public Proxy(final String localPath, final String base, final String me) {
+    private final Log log = LogFactory.getLog(getClass());
+    private String base;
+    private final String me;
+
+    public Proxy(final String localPath, final String base, final String me) {
         super(localPath + "/{path*}", null);
         setBase(base);
         this.me = me;
     }
-	
-	public Proxy(final String base, final String me) {
-		this("", base, me);
-	}
+
+    public Proxy(final String base, final String me) {
+        this("", base, me);
+    }
 
     public void setBase(String base) {
-		this.base = stripTrailingSlash(base);
+        this.base = stripTrailingSlash(base);
     }
 
     public String getBase() {
         return base;
     }
 
-	@Override
-	public Response get(Request req) {
-		return proxyRequest(req, new GetMethod());
-	}
-
     @Override
-	public Response delete(Request req){
-		return proxyRequest(req, new DeleteMethod());
+    public Response get(Request req) {
+        return proxyRequest(req, new GetMethod());
     }
 
-	@Override
-	public Response put(Request req) {
-		PutMethod m = new PutMethod();
+    @Override
+    public Response delete(Request req) {
+        return proxyRequest(req, new DeleteMethod());
+    }
 
-		setRequestRepresentation(req, m);
-		return proxyRequest(req, m);
-	}
+    @Override
+    public Response put(Request req) {
+        PutMethod m = new PutMethod();
 
-	protected void setRequestRepresentation(Request req, EntityEnclosingMethod method){
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		req.representation().write(out);
-		method.setRequestEntity(new InputStreamRequestEntity(new ByteArrayInputStream(out.toByteArray())));
-	}
-	
-	@Override
-	public Response post(Request req) {
-		PostMethod m = new PostMethod();
+        setRequestRepresentation(req, m);
+        return proxyRequest(req, m);
+    }
 
-		setRequestRepresentation(req, m);
-		return proxyRequest(req, m);
-	}
-	
-	protected String getQuery(Request req){
-	    return req.query().toString();
-	}
-	
-	protected String processUrl(String url){
-	    return url;
-	}
-	
-	protected Response proxyRequest(Request req, final HttpMethodBase method) {
-		method.setFollowRedirects(false);
-		
-		String path = req.path().valueFor("path");
-		if(!path.startsWith("/")) path = "/" + path;
-		String query = getQuery(req);
-		if(query==null){
-			query = "";
-		}else{
-			query = "?" + query;
-		}
-		String url = base + path + query;
-		log.debug("doing a " + method.getClass().getSimpleName() + " for " + url);
+    @Override
+    public Response patch(Request req) {
+        PatchMethod m = new PatchMethod();
+
+        setRequestRepresentation(req, m);
+        return proxyRequest(req, m);
+    }
+
+    protected void setRequestRepresentation(Request req, EntityEnclosingMethod method) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        req.representation().write(out);
+        method.setRequestEntity(new InputStreamRequestEntity(new ByteArrayInputStream(out.toByteArray())));
+    }
+
+    @Override
+    public Response post(Request req) {
+        PostMethod m = new PostMethod();
+
+        setRequestRepresentation(req, m);
+        return proxyRequest(req, m);
+    }
+
+    protected String getQuery(Request req) {
+        return req.query().toString();
+    }
+
+    protected String processUrl(String url) {
+        return url;
+    }
+
+    protected Response proxyRequest(Request req, final HttpMethodBase method) {
+        method.setFollowRedirects(false);
+
+        String path = req.path().valueFor("path");
+        if (!path.startsWith("/")) path = "/" + path;
+        String query = getQuery(req);
+        if (query == null) {
+            query = "";
+        } else {
+            query = "?" + query;
+        }
+        String url = base + path + query;
+        log.debug("doing a " + method.getClass().getSimpleName() + " for " + url);
 //		log.debug("Content type is " + req.representation().contentType());
-		try {
-			method.setURI(new URI(processUrl(url), true));
-		} catch (URIException e1) {
-			throw new RuntimeException("Error with uri: " + url, e1);
-		}
+        try {
+            method.setURI(new URI(processUrl(url), true));
+        } catch (URIException e1) {
+            throw new RuntimeException("Error with uri: " + url, e1);
+        }
 
         addRequestHeaders(req, method);
-        
-        if(req.representation().contentType()!=null){
-        	method.addRequestHeader("Content-Type", req.representation().contentType());
+
+        if (req.representation().contentType() != null) {
+            method.addRequestHeader("Content-Type", req.representation().contentType());
         }
-		
-		for(Header next : method.getRequestHeaders()){
-			log.debug("Sending header: " + next);
-		}
+
+        for (Header next : method.getRequestHeaders()) {
+            log.debug("Sending header: " + next);
+        }
 
         HttpClient client = createHttpClient();
 
         return executeMethod(client, method, req);
 
-	}
+    }
 
     protected Response executeMethod(HttpClient client, HttpMethodBase method, Request req) {
         try {
             int codeValue = client.executeMethod(method);
 
             ResponseCode responseCode = ResponseCode.forCode(codeValue);
-            if(responseCode==null){
+            if (responseCode == null) {
                 log.error("Unknown response code: " + codeValue);
-            }else{
+            } else {
                 log.debug("Response was " + responseCode);
             }
 
@@ -184,9 +175,9 @@ public class Proxy extends HttpObject {
             return createResponse(method, responseCode, headersReturned);
 
         } catch (Exception e) {
-			log.error("Error proxying", e);
-			return BAD_GATEWAY();
-		}
+            log.error("Error proxying", e);
+            return BAD_GATEWAY();
+        }
     }
 
     protected HttpClient createHttpClient() {
@@ -194,28 +185,26 @@ public class Proxy extends HttpObject {
     }
 
     protected void addRequestHeaders(Request req, final HttpMethodBase method) {
-        for(HeaderField next : req.header().fields()) {
-        	method.addRequestHeader(next.name(), next.value());
+        for (HeaderField next : req.header().fields()) {
+            method.addRequestHeader(next.name(), next.value());
         }
     }
 
-    
-    
     protected List<HeaderField> extractResponseHeaders(HttpMethodBase method) {
         List<HeaderField> headersReturned = new ArrayList<HeaderField>();
-        for(Header h : method.getResponseHeaders()){
+        for (Header h : method.getResponseHeaders()) {
             log.debug("Found header: " + h.getName() + "=" + h.getValue());
             String name = h.getName();
             String value = h.getValue();
-            if(name.equals("Set-Cookie")){
-            	SetCookieField setCookieField = SetCookieField.fromHeaderValue(value);
+            if (name.equals("Set-Cookie")) {
+                SetCookieField setCookieField = SetCookieField.fromHeaderValue(value);
                 log.debug("Cookie found: " + setCookieField);
                 headersReturned.add(setCookieField);
-            }else if(name.equals("Location")){
+            } else if (name.equals("Location")) {
                 String a = processRedirect(value);
                 log.debug("Redirecting to " + a);
                 headersReturned.add(new LocationField(a));
-            }else {
+            } else {
                 headersReturned.add(new GenericHeaderField(name, value));
             }
         }
@@ -228,39 +217,52 @@ public class Proxy extends HttpObject {
     }
 
     protected Response createResponse(final HttpMethodBase method, ResponseCode responseCode, List<HeaderField> headersReturned) {
-        return new Response(responseCode, new Representation(){
+        return new Response(responseCode, new Representation() {
             @Override
             public String contentType() {
                 Header h = method.getResponseHeader("Content-Type");
-                return h==null?null:h.getValue();
+                return h == null ? null : h.getValue();
             }
 
             @Override
             public void write(OutputStream out) {
                 try {
-                	if(method.getResponseBodyAsStream() != null){
-                		
-                		byte[] buffer = new byte[1024];
-                		InputStream in = method.getResponseBodyAsStream();
-                		for(int x=in.read(buffer);x!=-1;x=in.read(buffer)){
-                			out.write(buffer, 0, x);
-                		}
-                	}
-                		
+                    if (method.getResponseBodyAsStream() != null) {
+
+                        byte[] buffer = new byte[1024];
+                        InputStream in = method.getResponseBodyAsStream();
+                        for (int x = in.read(buffer); x != -1; x = in.read(buffer)) {
+                            out.write(buffer, 0, x);
+                        }
+                    }
+
                 } catch (IOException e) {
-                	e.printStackTrace();
+                    e.printStackTrace();
                     throw new RuntimeException("Error writing response", e);
                 }
             }
         },
-        headersReturned.toArray(new HeaderField[]{}));
+                headersReturned.toArray(new HeaderField[]{}));
     }
 
-    protected static final String stripTrailingSlash(String text){
-		if(text.endsWith("/") && text.length()>1){
-			return text.substring(0, text.length()-1);
-		}else{
-			return text;
-		}
-	}
+    protected static final String stripTrailingSlash(String text) {
+        if (text.endsWith("/") && text.length() > 1) {
+            return text.substring(0, text.length() - 1);
+        } else {
+            return text;
+        }
+    }
+
+    private static class PatchMethod extends EntityEnclosingMethod {
+        public PatchMethod() {
+        }
+
+        private PatchMethod(String uri) {
+            super(uri);
+        }
+
+        public String getName() {
+            return "PATCH";
+        }
+    }
 }
