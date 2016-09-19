434a21ac82e526d73c00f0a143b2463eda5d9e02
Sean Shubin <sshubin@cj.com>
2016-09-19 16:05:18 -0700

    add HttpOnly field to SetCookieField

--- core/src/main/java/org/httpobjects/header/response/SetCookieField.java
+++ core/src/main/java/org/httpobjects/header/response/SetCookieField.java
@@ -1,28 +1,35 @@
 /**
+ * <p>
  * Copyright (C) 2011, 2012 Commission Junction Inc.
- *
+ * </p>
+ * <p>
  * This file is part of httpobjects.
- *
+ * </p>
+ * <p>
  * httpobjects is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- *
+ * </p>
+ * <p>
  * httpobjects is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- *
+ * </p>
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with httpobjects; see the file COPYING.  If not, write to the
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- *
+ * </p>
+ * <p>
  * Linking this library statically or dynamically with other modules is
  * making a combined work based on this library.  Thus, the terms and
  * conditions of the GNU General Public License cover the whole
  * combination.
- *
+ * </p>
+ * <p>
  * As a special exception, the copyright holders of this library give you
  * permission to link this library with independent modules to produce an
  * executable, regardless of the license terms of these independent
@@ -34,108 +41,123 @@
  * this exception to your version of the library, but you are not
  * obligated to do so.  If you do not wish to do so, delete this
  * exception statement from your version.
+ * </p>
  */
 package org.httpobjects.header.response;
-
-import java.util.StringTokenizer;
 
 import org.httpobjects.header.HeaderField;
 import org.httpobjects.header.HeaderFieldVisitor;
 
+import java.util.StringTokenizer;
+
 public class SetCookieField extends HeaderField {
-	public static final SetCookieField fromHeaderValue(String header){
-		StringTokenizer s = new StringTokenizer(header);
-		NameValue nameValue = NameValue.parse(s.nextToken(";"));
+    public static final SetCookieField fromHeaderValue(String header) {
+        StringTokenizer s = new StringTokenizer(header);
+        NameValue nameValue = NameValue.parse(s.nextToken(";"));
 
-		String domain = null;
-		String path = null;
-		String expiration = null;
-		Boolean secure = null;
+        String domain = null;
+        String path = null;
+        String expiration = null;
+        Boolean secure = null;
+        Boolean httpOnly = null;
 
-		while(s.hasMoreTokens()){
-			String next = s.nextToken(";");
-			if(next!=null){
-				next = next.trim();
-				if(next.equals("Secure")){
-					secure = true;
-				}else{
-					NameValue property = NameValue.parse(next.trim());
-					String name = property.name.toLowerCase();
+        while (s.hasMoreTokens()) {
+            String next = s.nextToken(";");
+            if (next != null) {
+                next = next.trim();
+                if (next.equals("Secure")) {
+                    secure = true;
+                } else if (next.equals("HttpOnly")) {
+                    httpOnly = true;
+                } else {
+                    NameValue property = NameValue.parse(next.trim());
+                    String name = property.name.toLowerCase();
 
-					if(name.equals("domain")){
-						domain = property.value;
-					}else if(name.equals("path")){
-						path = property.value;
-					}else if(name.equals("expires")){
-						expiration = property.value;
-					}
-				}
-			}
-		}
-		return new SetCookieField(nameValue.name, nameValue.value, domain, path, expiration, secure);
-	}
+                    if (name.equals("domain")) {
+                        domain = property.value;
+                    } else if (name.equals("path")) {
+                        path = property.value;
+                    } else if (name.equals("expires")) {
+                        expiration = property.value;
+                    }
+                }
+            }
+        }
+        return new SetCookieField(nameValue.name, nameValue.value, domain, path, expiration, secure, httpOnly);
+    }
 
-	private static class NameValue {
-		public static NameValue parse(String attributeString){
-			try {
-				final String name, value;
-				
-				final int pos = attributeString.indexOf('=');
-				if(pos == -1){
-					name = attributeString;
-					value = null;
-				}else{
-					name = attributeString.substring(0, pos);
-					value = stripQuotes(attributeString.substring(pos + 1));
-				}
-				
-				return new NameValue(name, value);
-			} catch (Exception e) {
-				throw new RuntimeException("Error parsing " + attributeString);
-			}
-		}
+    public boolean isSecure() {
+        return secure == null ? false : secure;
+    }
 
-		private static String stripQuotes(String value) {
-			if(value.startsWith("\"") && value.endsWith("\"")){
-				value = value.substring(1, value.length()-1);
-			}
-			return value;
-		}
-		private final String name, value;
+    private static class NameValue {
+        public static NameValue parse(String attributeString) {
+            try {
+                final String name, value;
 
-		public NameValue(String name, String value) {
-			super();
-			this.name = name;
-			this.value = value;
-		}
-	}
+                final int pos = attributeString.indexOf('=');
+                if (pos == -1) {
+                    name = attributeString;
+                    value = null;
+                } else {
+                    name = attributeString.substring(0, pos);
+                    value = stripQuotes(attributeString.substring(pos + 1));
+                }
 
-	public final String name;
+                return new NameValue(name, value);
+            } catch (Exception e) {
+                throw new RuntimeException("Error parsing " + attributeString);
+            }
+        }
+
+        private static String stripQuotes(String value) {
+            if (value.startsWith("\"") && value.endsWith("\"")) {
+                value = value.substring(1, value.length() - 1);
+            }
+            return value;
+        }
+
+        private final String name, value;
+
+        public NameValue(String name, String value) {
+            super();
+            this.name = name;
+            this.value = value;
+        }
+    }
+
+    public final String name;
     public final String value;
     public final String domain;
     public final String path;
     public final String expiration;
     public final Boolean secure;
+    public final Boolean httpOnly;
 
     public SetCookieField(String name, String value, String domain, String path,
-			String expiration, Boolean secure) {
-		super();
-		this.name = name;
-		this.value = value;
-		this.domain = domain;
-		this.path = path;
-		this.expiration = expiration;
-		this.secure = secure;
-	}
-    
+                          String expiration, Boolean secure, Boolean httpOnly) {
+        this.name = name;
+        this.value = value;
+        this.domain = domain;
+        this.path = path;
+        this.expiration = expiration;
+        this.secure = secure;
+        this.httpOnly = httpOnly;
+    }
+
+    public SetCookieField(String name, String value, String domain, String path,
+                          String expiration, Boolean secure) {
+        this(name, value, domain, path, expiration, secure, null);
+    }
+
     public SetCookieField(String name, String value, String domain) {
-		this(name, value, domain, null, null, null);
-	}
-    
-	@Override
-	public <T> T accept(HeaderFieldVisitor<T> visitor) {
-		return visitor.visit(this);
-	}
+        this(name, value, domain, null, null, null);
+    }
+
+    @Override
+    public <T> T accept(HeaderFieldVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
 
     @Override
     public String name() {
@@ -146,24 +168,28 @@ public class SetCookieField extends HeaderField {
     public String value() {
         return toString();
     }
-    
 
-	@Override
-	public String toString() {
-		String base = name + "=" + value;
-		
-		String secure = (this.secure!=null && this.secure)?" Secure;":"";
-		
-		
-		String tail = appendFieldIfNotNull("Domain", domain) + 
-		appendFieldIfNotNull("Path", path) + 
-		appendFieldIfNotNull("Expires", expiration) + 
-		secure;
-		
-		return tail.isEmpty()?base:base + ";" + tail;
-	}
 
-	private String appendFieldIfNotNull(String name, String value) {
-		return value==null?"":" " + name + "=" + value + ";";
-	}
+    @Override
+    public String toString() {
+        String base = name + "=" + value;
+
+        String secure = (this.secure != null && this.secure) ? " Secure;" : "";
+
+
+        String tail = appendFieldIfNotNull("Domain", domain) +
+                appendFieldIfNotNull("Path", path) +
+                appendFieldIfNotNull("Expires", expiration) +
+                secure;
+
+        return tail.isEmpty() ? base : base + ";" + tail;
+    }
+
+    private String appendFieldIfNotNull(String name, String value) {
+        return value == null ? "" : " " + name + "=" + value + ";";
+    }
+
+    public boolean isHttpOnly() {
+        return httpOnly == null ? false : httpOnly;
+    }
 }
--- core/src/test/java/org/httpobjects/header/response/SetCookieFieldTest.java
+++ core/src/test/java/org/httpobjects/header/response/SetCookieFieldTest.java
@@ -1,28 +1,35 @@
 /**
+ * <p>
  * Copyright (C) 2011, 2012 Commission Junction Inc.
- *
+ * </p>
+ * <p>
  * This file is part of httpobjects.
- *
+ * </p>
+ * <p>
  * httpobjects is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- *
+ * </p>
+ * <p>
  * httpobjects is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- *
+ * </p>
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with httpobjects; see the file COPYING.  If not, write to the
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- *
+ * </p>
+ * <p>
  * Linking this library statically or dynamically with other modules is
  * making a combined work based on this library.  Thus, the terms and
  * conditions of the GNU General Public License cover the whole
  * combination.
- *
+ * </p>
+ * <p>
  * As a special exception, the copyright holders of this library give you
  * permission to link this library with independent modules to produce an
  * executable, regardless of the license terms of these independent
@@ -34,17 +41,19 @@
  * this exception to your version of the library, but you are not
  * obligated to do so.  If you do not wish to do so, delete this
  * exception statement from your version.
+ * </p>
  */
 package org.httpobjects.header.response;
 
-import org.junit.Assert;
-
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+//https://tools.ietf.org/html/rfc6265
 public class SetCookieFieldTest {
 
     @Test
-    public void valuesMayBeQuoted(){
+    public void valuesMayBeQuoted() {
 
         // given
         String value = "foo=\"bar baz\"; path=\"/xyz\"; domain=\"xyz.com\"; Expires=\"Wed, 13-Jan-2021 22:23:01 GMT\"";
@@ -52,87 +61,166 @@ public class SetCookieFieldTest {
         SetCookieField c = SetCookieField.fromHeaderValue(value);
 
         // then
-        Assert.assertEquals("foo", c.name);
-        Assert.assertEquals("bar baz", c.value);
-        Assert.assertEquals("/xyz", c.path);
-        Assert.assertEquals("xyz.com", c.domain);
-        Assert.assertEquals("Wed, 13-Jan-2021 22:23:01 GMT", c.expiration);
+        assertEquals("foo", c.name);
+        assertEquals("bar baz", c.value);
+        assertEquals("/xyz", c.path);
+        assertEquals("xyz.com", c.domain);
+        assertEquals("Wed, 13-Jan-2021 22:23:01 GMT", c.expiration);
     }
 
     @Test
-    public void attributeNamesAreCaseInsentitive(){
-        
+    public void attributeNamesAreCaseInsentitive() {
+
         // given
         String[] caseVariations = {"path", "PATH", "pAtH", "PaTh"};
-        
-        for(String pathAttributeName : caseVariations){
-            
+
+        for (String pathAttributeName : caseVariations) {
+
             // when
             SetCookieField c = SetCookieField.fromHeaderValue("foo=bar; " + pathAttributeName + "=/");
-            
+
             // then
-            Assert.assertEquals("foo", c.name);
-            Assert.assertEquals("bar", c.value);
-            Assert.assertEquals("/", c.path);
+            assertEquals("foo", c.name);
+            assertEquals("bar", c.value);
+            assertEquals("/", c.path);
         }
     }
-    
+
 
     @Test
-    public void attributeValuesAreOptional(){
+    public void attributeValuesAreOptional() {
 
         // given
         String value = "foo=\"bar baz\"; ThisAttributeHasNoValue";
-    
+
         // when
         SetCookieField c = SetCookieField.fromHeaderValue(value);
 
         // then
-        Assert.assertEquals("foo", c.name);
-        Assert.assertEquals("bar baz", c.value);
+        assertEquals("foo", c.name);
+        assertEquals("bar baz", c.value);
     }
-    
-    
-	@Test
-	public void basicNameValueSetCookieField(){
-		SetCookieField c = SetCookieField.fromHeaderValue("name=value");
-		assertSame(
-				new SetCookieField("name", "value", null),
-				c
-		);
-		
-		Assert.assertEquals("name=value", c.toString());
-	}
-	
-	@Test
-	public void basicSetCookieFieldWithDomain(){
-		SetCookieField c = SetCookieField.fromHeaderValue("LSID=DQAAAK…Eaem_vYg; Domain=docs.foo.com");
-		assertSame(
-				new SetCookieField("LSID", "DQAAAK…Eaem_vYg", "docs.foo.com"),
-				c
-		);
 
-		Assert.assertEquals("LSID=DQAAAK…Eaem_vYg; Domain=docs.foo.com;", c.toString());
-	}
-	
 
-	@Test
-	public void full(){
-		SetCookieField c = SetCookieField.fromHeaderValue("SSID=Ap4P….GTEq; Domain=.foo.com; Path=/; Expires=Wed, 13-Jan-2021 22:23:01 GMT; Secure;");
-		assertSame(
-				new SetCookieField("SSID", "Ap4P….GTEq", ".foo.com", "/", "Wed, 13-Jan-2021 22:23:01 GMT", true),
-				c
-		);
-		System.out.println(c);
-		Assert.assertEquals("SSID=Ap4P….GTEq; Domain=.foo.com; Path=/; Expires=Wed, 13-Jan-2021 22:23:01 GMT; Secure;", c.toString());
-		
-	}
-	private void assertSame(SetCookieField expected, SetCookieField actual){
-		Assert.assertEquals(expected.name, actual.name);
-		Assert.assertEquals(expected.value, actual.value);
-		Assert.assertEquals(expected.domain, actual.domain);
-		Assert.assertEquals(expected.path, actual.path);
-		Assert.assertEquals(expected.expiration, actual.expiration);
-		Assert.assertEquals(expected.secure, actual.secure);
-	}
+    @Test
+    public void basicNameValueSetCookieField() {
+        SetCookieField c = SetCookieField.fromHeaderValue("name=value");
+        assertSame(
+                new SetCookieField("name", "value", null),
+                c
+        );
+
+        assertEquals("name=value", c.toString());
+    }
+
+    @Test
+    public void basicSetCookieFieldWithDomain() {
+        SetCookieField c = SetCookieField.fromHeaderValue("LSID=DQAAAK…Eaem_vYg; Domain=docs.foo.com");
+        assertSame(
+                new SetCookieField("LSID", "DQAAAK…Eaem_vYg", "docs.foo.com"),
+                c
+        );
+
+        assertEquals("LSID=DQAAAK…Eaem_vYg; Domain=docs.foo.com;", c.toString());
+    }
+
+
+    @Test
+    public void full() {
+        SetCookieField c = SetCookieField.fromHeaderValue("SSID=Ap4P….GTEq; Domain=.foo.com; Path=/; Expires=Wed, 13-Jan-2021 22:23:01 GMT; Secure;");
+        assertSame(
+                new SetCookieField("SSID", "Ap4P….GTEq", ".foo.com", "/", "Wed, 13-Jan-2021 22:23:01 GMT", true),
+                c
+        );
+        System.out.println(c);
+        assertEquals("SSID=Ap4P….GTEq; Domain=.foo.com; Path=/; Expires=Wed, 13-Jan-2021 22:23:01 GMT; Secure;", c.toString());
+
+    }
+
+    @Test
+    public void parseHttpOnlyTrue() {
+        // given
+        String headerValue = "name=value; HttpOnly;";
+
+        // when
+        SetCookieField setCookieField = SetCookieField.fromHeaderValue(headerValue);
+
+        // then
+        assertTrue(setCookieField.httpOnly);
+    }
+
+    @Test
+    public void parseHttpOnlyFalse() {
+        // given
+        String headerValue = "name=value;";
+
+        // when
+        SetCookieField setCookieField = SetCookieField.fromHeaderValue(headerValue);
+
+        // then
+        assertNull(setCookieField.httpOnly);
+    }
+
+    @Test
+    public void getterForHttpOnlyTrue() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, null, Boolean.TRUE);
+
+        // then
+        assertTrue(cookieField.isHttpOnly());
+    }
+
+    @Test
+    public void getterForHttpOnlyFalse() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, null, Boolean.FALSE);
+
+        // then
+        assertFalse(cookieField.isHttpOnly());
+    }
+
+    @Test
+    public void getterForHttpOnlyNull() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, null, null);
+
+        // then
+        assertFalse(cookieField.isHttpOnly());
+    }
+
+    @Test
+    public void getterForSecureTrue() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, Boolean.TRUE, null);
+
+        // then
+        assertTrue(cookieField.isSecure());
+    }
+
+    @Test
+    public void getterForSecureFalse() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, Boolean.FALSE, null);
+
+        // then
+        assertFalse(cookieField.isSecure());
+    }
+
+    @Test
+    public void getterForSecureNull() {
+        // given
+        SetCookieField cookieField = new SetCookieField(null, null, null, null, null, null, null);
+
+        // then
+        assertFalse(cookieField.isSecure());
+    }
+
+    private void assertSame(SetCookieField expected, SetCookieField actual) {
+        assertEquals(expected.name, actual.name);
+        assertEquals(expected.value, actual.value);
+        assertEquals(expected.domain, actual.domain);
+        assertEquals(expected.path, actual.path);
+        assertEquals(expected.expiration, actual.expiration);
+        assertEquals(expected.secure, actual.secure);
+    }
 }
